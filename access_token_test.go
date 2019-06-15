4dea2d418e7ef9b06f9347374a5ba44467379632
fang duan <df1228@gmail.com>
2019-06-15 19:42:54 +0800

    Add global beforeEach (BeforeSuite)

--- access_token_test.go
+++ access_token_test.go
@@ -1,24 +1,11 @@
 package WechatWork_test
 
 import (
-	"os"
-	"strconv"
-
-	WechatWork "github.com/dfang/wechat-work-go"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Access Token", func() {
-	corpID := os.Getenv("CORP_ID")
-	corpSecret := os.Getenv("CORP_SECRET")
-	agentID, _ := strconv.ParseInt(os.Getenv("AGENT_ID"), 10, 64)
-	var app *WechatWork.App
-
-	BeforeEach(func() {
-		client := WechatWork.New(corpID)
-		app = client.WithApp(corpSecret, agentID)
-	})
 
 	It("should get access token", func() {
 		app.SpawnAccessTokenRefresher()
--- wechat_work_go_suite_test.go
+++ wechat_work_go_suite_test.go
@@ -1,16 +1,30 @@
 package WechatWork_test
 
 import (
+	"os"
+	"strconv"
 	"testing"
 
+	WechatWork "github.com/dfang/wechat-work-go"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
 	_ "github.com/joho/godotenv/autoload"
 )
 
+var app *WechatWork.App
+
 func TestWechatWorkGo(t *testing.T) {
 
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "WechatWorkGo Suite")
 }
+
+var _ = BeforeSuite(func() {
+	corpID := os.Getenv("CORP_ID")
+	corpSecret := os.Getenv("CORP_SECRET")
+	agentID, _ := strconv.ParseInt(os.Getenv("AGENT_ID"), 10, 64)
+
+	client := WechatWork.New(corpID)
+	app = client.WithApp(corpSecret, agentID)
+})
