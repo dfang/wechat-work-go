66602453f2abd393d05e90d999c1ae7e14df9292
fang duan <df1228@gmail.com>
2019-06-22 08:38:50 +0800

    Clean

--- client.go
+++ client.go
@@ -27,6 +27,7 @@ type App struct {
 
 	// CorpSecret 应用的凭证密钥，其实应该叫AgentSecret更好，必填
 	CorpSecret string
+
 	// AgentID 应用 ID，必填
 	AgentID int64
 
@@ -40,12 +41,7 @@ type App struct {
 	Cache cache.Cache
 }
 
-// type Context struct {
-// 	Client resty.Client
-// 	App    *App
-// }
-
-// NewCorp 构造一个 WechatWork 对象，需要提供企业 ID
+// New 构造一个 WechatWork 对象，需要提供企业 ID
 //
 // 通常，要使用wechat-work-go, 你需要先创建一个 WechatWork 的对象，
 // 接着以此对象调用 WithApp 创建一个app
@@ -56,7 +52,7 @@ type App struct {
 // 简单来说就是 a 应用的CORP_SECRET 和 AGENT_ID 获取的access_token 是不能操作 b应用的
 //
 // 	CORP_ID 去企业微信管理中的 我的企业 最底部
-// 	CORP_SECRET 其实是应用的secret，个人应该叫AgentSecret 或 AppSecret 更合适, 但因为api接口和官方文档叫corpSecret，所以不改变
+// 	CORP_SECRET 其实是应用的secret，个人感觉应该叫AgentSecret 或 AppSecret 更合适, 但因为api接口和官方文档叫corpSecret，所以不改变
 // 	AGENT_ID 应用的ID， CORP_SECRET 和 AGENT_ID 都去应用的详情页面找
 //
 // 示例代码:
@@ -98,55 +94,11 @@ func newDefaultRestyClient() *resty.Client {
 	return client
 }
 
-// NewRequest return resty.Request with right url, right configuration
-func (app *App) NewRequest(path string, qs urlValuer, withAccessToken bool) *resty.Request {
-	client := newDefaultRestyClient()
-
-	values := url.Values{}
-	if valuer, ok := qs.(urlValuer); ok {
-		values = valuer.IntoURLValues()
-	}
-
-	fmt.Println("with token is true")
-	if withAccessToken {
-		fmt.Println("spawn access token refresher")
-		// app.SpawnAccessTokenRefresher()
-		// app.SyncAccessToken()
-
-		// for {
-		// 	if app.AccessToken != "" {
-		// 		// if values.Get("access_token") != "" {
-		// 		values.Set("access_token", app.AccessToken)
-		// 		// } else {
-		// 		// 	values.Add("access_token", app.AccessToken)
-		// 		// }
-		// 		break
-		// 	}
-		// }
-		token := app.GetAccessToken()
-		values.Set("access_token", token)
-	}
-
-	url := path + "?" + values.Encode()
-	req := client.NewRequest()
-	req.URL = url
-	return req
-}
-
-// // Get 一切get请求的api调用可使用此方法
-// //
-// // 企业微信中，获取操作和删除都是GET请求
-// func (app *App) Get(path string, qs urlValuer, respObj interface{}, withAccessToken bool) error {
-// 	req := app.NewRequest(path, qs, withAccessToken)
-// 	resp, err := req.SetResult(&respObj).Get(req.URL)
-// 	if err != nil {
-// 		fmt.Fprintln(os.Stdout, resp.Body())
-// 		panic(err)
-// 	}
-// 	return nil
-// }
-
-// SimpleGet just like resty.SetReult(&respObj).Get(url)
+// SimpleGet 一切get请求的api调用可使用此方法
+//
+// just like resty.SetReult(&respObj).Get(url)
+//
+// 企业微信中，获取操作和删除都是GET请求
 //
 // note: url must be full
 func (app *App) SimpleGet(url string, respObj interface{}) error {
@@ -162,27 +114,11 @@ func (app *App) SimpleGet(url string, respObj interface{}) error {
 	return nil
 }
 
-// // Post 一切Post请求的api调用使用此方法
-// //
-// // 企业微信中，删除操作一般都是GET请求，更新操作、批量删除成员是POST请求，没有PUT、PATCH、DELETE
-// func (app *App) Post(path string, qs urlValuer, body bodyer, respObj interface{}, withAccessToken bool) error {
-// 	b, _ := body.IntoBody()
-// 	// TODO
-// 	req := app.NewRequest(path, qs, withAccessToken)
-// 	resp, err := req.
-// 		SetHeader("Content-Type", "application/json").
-// 		SetBody(b).
-// 		SetResult(&respObj).
-// 		Post(req.URL)
-
-// 	if err != nil {
-// 		fmt.Fprintln(os.Stdout, resp.Body())
-// 		panic(err)
-// 	}
-// 	return nil
-// }
-
-// SimplePost just like resty.SetBody(b).SetReult(&respObj).Post(url)
+// SimplePost 一切Post请求的api调用使用此方法
+//
+// just like resty.SetBody(b).SetReult(&respObj).Post(url)
+//
+// 企业微信中，删除操作一般都是GET请求，更新操作、批量删除成员是POST请求，没有PUT、PATCH、DELETE
 //
 // resty can Automatically marshal and unmarshal
 // note: url must be full
